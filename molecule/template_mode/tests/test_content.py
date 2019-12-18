a2c56ac05ceb19f03dfe4ad8fee58db1dddaca57
Gregory Hellings <greg.hellings@gmail.com>
2019-12-18 09:52:19 -0600

    Create template_mode scenario

--- molecule/template_mode/tests/test_content.py
+++ molecule/template_mode/tests/test_content.py
@@ -6,3 +6,13 @@
 # Most tests should be written in the shared/tests directory so that they can
 # be captured by all the scenarios. Only add tests here if there are tests
 # only relevant to a particular scenario
+
+
+def test_file_a(host):
+    content = host.file("/out/file.yml").content
+    assert content == b'key: "my string"\n'
+
+
+def test_file_raw(host):
+    content = host.file("/out/raw.in").content
+    assert content == b'key: {{ value }}\n'
